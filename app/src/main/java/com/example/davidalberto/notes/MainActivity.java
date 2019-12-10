d4fdc989fc68fb4d97ade45eb7f3e946d389cd65
David <david_fiallos@hotmail.com>
2019-12-10 14:23:55 -0600

    5

--- app/src/main/java/com/example/davidalberto/notes/MainActivity.java
+++ app/src/main/java/com/example/davidalberto/notes/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.davidalberto.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ADD = Menu.FIRST;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
