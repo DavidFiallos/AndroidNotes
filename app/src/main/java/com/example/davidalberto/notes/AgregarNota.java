e473398a719c847d6ff4f21b263f92b257b7cddc
David <david_fiallos@hotmail.com>
2019-12-10 22:21:04 -0600

    6

--- app/src/main/java/com/example/davidalberto/notes/AgregarNota.java
+++ app/src/main/java/com/example/davidalberto/notes/AgregarNota.java
@@ -1,4 +1,31 @@
 package com.example.davidalberto.notes;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class AgregarNota {
+public class AgregarNota extends AppCompatActivity {
+
+    Button Add;
+    String type;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agregar_nota);
+
+        Add = (Button)findViewById(R.id.btn_agregar);
+
+        Bundle bundle = this.getIntent().getExtras();
+        type = bundle.getString("type");
+
+        if(type.equals("add")){
+
+            Add.setText("Agregar nota");
+
+        }
+
+    }
+
 }
